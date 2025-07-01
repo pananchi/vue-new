a0bb092e1a4debf5db583c30e972fba1fbb7c5af
“Angel” <anm30256@gmail.com>
2025-07-01 15:13:34 +0800

    建立及使用組件

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
+import FirstComponent from "./components/FirstComponent.vue";
 
 </script>
 
 <template>
-<h2>Hello,Vue!!</h2>
+  <h2>Hello,Vue!!</h2>
+  <firstComponent />
 </template>
 
 <style scoped>
-
+  h2{
+    color: blue;
+  }
 </style>
---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <h2>第一個 Component</h2>
+</template>
+
+<style scoped>
+h2{
+  color: orange;
+}
+</style>
+
+<script setup>
+</script>
