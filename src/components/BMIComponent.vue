613cdf7906794488510538a019a89016b1412d91
“Angel” <anm30256@gmail.com>
2025-07-03 18:21:19 +0800

    BMI 計算使用 computed

--- src/components/BMIComponent.vue
+++ src/components/BMIComponent.vue
@@ -1,24 +1,24 @@
 <script setup>
 import { computed, ref } from 'vue';
 
-const height = ref(0);
-const weight = ref(0);
-const calculate = computed(() => weight.value / (( height.value / 100 ) ** 2));
-const bmiDisplay = computed(() => calculate.value.toFixed(2));
+const height = ref(170);
+const weight = ref(70);
+const bmi = computed(() => (weight.value / (( height.value / 100 ) ** 2)).toFixed(2));
 </script>
 
 <template>
   <div>
     <!-- 我做的 -->
-     <label>身高(公分)：<input type="text" v-model.lazy="height" /></label>
+     <!-- <label>身高(公分)：<input type="text" v-model.lazy="height" /></label>
       <br>
       <label>體重(公斤)：<input type="text" v-model.lazy="weight" /></label>
-      <p>BMI：{{ bmiDisplay }}</p>
+      <p>BMI：{{ bmi }}</p> -->
 
       <!-- 老師做的 -->
-       <div><label>體重(kg)：<input type="number" name="weight" v-model.lazy="weight"></label></div>
-       <div><label>身高(cm)：<input type="number" name="height" v-model.lazy="height"></label></div>
-       <p>BMI：{{  }}</p>
+       <div><label>體重(kg)：<input type="number" name="weight" v-model.lazy.number="weight"></label></div>
+       <div><label>身高(cm)：<input type="number" name="height" v-model.lazy.number="height"></label></div>
+       <!-- <p>BMI：{{ (weight / (( height / 100 ) ** 2)) .toFixed(2) }}</p> -->
+       <p>BMI：{{ bmi }}</p>
   </div>
 </template>
 
