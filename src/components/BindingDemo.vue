2b9167a245c81881465d23bcfff6e4f5c4c9bca6
“Angel” <anm30256@gmail.com>
2025-07-03 14:53:02 +0800

    靜態檔案放在public與assets的使用方式

--- src/components/BindingDemo.vue
+++ src/components/BindingDemo.vue
@@ -1,9 +1,12 @@
 <script setup>
 import { ref } from 'vue';
-
+import mycat from '@/assets/images/cat02.jpg'; // 建議匯入方式
   
-  const userName= ref('Jack');
-  const isAbmin = ref(false);
+const userName= ref('Jack');
+const isAbmin = ref(false);
+const dog = ref("/images/dog01.jpg")
+const cat = ref("/src/assets/images/cat02.jpg")
+
 </script>
 
 <template>
@@ -13,9 +16,17 @@ import { ref } from 'vue';
     <p> {{ userName || '訪客' }}</p>
     <!-- 如果isAdmin為true就顯示管理者登入，如果為false就不顯示任何內容 -->
     <p> {{ isAbmin ? '管理員登入':'' }}</p>
+    <img :src= "dog" />
+    <!-- assets 裡的靜態檔案 -->
+    <img src="@/assets/images/cat02.jpg" title="不建議這樣使用"/>
+    <img :src="cat" title="不建議這樣使用" /> 
+    <img :src="mycat" title="建議這樣使用" />
   </div>
 </template>
 
 <style lang="css" scoped>
+img{
+  width: 100px;
+}
 
 </style>
