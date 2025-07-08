099693eb043a98dd3cd23fc46a2ee620a54afaab
“Angel” <anm30256@gmail.com>
2025-07-08 15:30:42 +0800

    使用 watch 監聽響應式資料

--- src/components/ClockComponents.vue
+++ src/components/ClockComponents.vue
@@ -1,23 +1,43 @@
 <script setup>
-import { ref } from 'vue';
+import { ref , watch } from 'vue';
   
-  const formatTime = (today) => {
-    const hours = today.getHours().toString().padStart(2, '0');
-    const minutes = today.getMinutes().toString().padStart(2, '0');
-    const seconds = today.getSeconds().toString().padStart(2, '0');
-    return `${hours}:${minutes}:${seconds}`;
-  };
+const formatTime = (today) => {
+  const hours = today.getHours().toString().padStart(2, '0');
+  const minutes = today.getMinutes().toString().padStart(2, '0');
+  const seconds = today.getSeconds().toString().padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}`;
+};
 
-  const clock = ref(formatTime(new Date()));
+const user = ref({
+  name:"Jack",
+  address:{
+    road:"台北路",
+    city:"台北市",
+  }
+})
 
-  window.setInterval(() => {
-    const today = new Date();
-    clock.value = formatTime(today);
-}, 1000);
+const clock = ref(formatTime(new Date()));
+
+
+
+watch(clock,(newTime, oldTime) => {
+  console.log(`Clock changed from ${oldTime} to ${newTime}`);
+},{immediate: true}); //{once: true}
+
+const changeTime = () => {
+  const today = new Date();
+  clock.value = formatTime(today);
+};
+
+// window.setInterval(() => {
+//   const today = new Date();
+//   clock.value = formatTime(today);
+// }, 1000);
 </script>
 
 <template>
   <div>
+    <button @click="changeTime">更新時間</button>
     <h2>{{ clock }}</h2>
   </div>
 </template>
