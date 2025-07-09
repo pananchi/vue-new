96770f5b0baa6031bae5a6bc395657d354642ffd
“Angel” <anm30256@gmail.com>
2025-07-09 16:28:49 +0800

    修改待辦事項內容

--- src/views/TodosView_Full.vue
+++ src/views/TodosView_Full.vue
@@ -1,106 +1,89 @@
 <script setup>
-// import TodoAdd from '@/components/TodoAdd.vue';
-// import TodoFooter from '@/components/TodoFooter.vue';
-// import { computed, ref, watchEffect } from 'vue';
-// import { useTodoStore } from '@/stores/todo';
-// const storeTodo = useTodoStore();
+import { computed, ref } from 'vue';
+const newTodo = ref('')
 
-// const todos = ref(
-//     [
-//         { "id": "m21uwqfprb0ncx4", "title": "買菜", "completed": false },
-//         { "id": "m21w6x73hw2tvrc", "title": "看電視", "completed": true },
-//         { "id": "m21w6x73hw2abcd", "title": "睡覺", "completed": true },
-//     ]
-// )
-//把資料寫進localStorage
-//localStorage.setItem('todos', JSON.stringify(todos.value));
-//從localStorage讀取待辦事項
-const todos = ref(JSON.parse(localStorage.getItem('todos')))
-
+const todos = ref(
+  [
+    { "id": "m21uwqfprb0ncx4", "title": "買菜", "completed": false },
+    { "id": "m21w6x73hw2tvrc", "title": "看電視", "completed": true },
+    { "id": "m21w6x73hw2abcd", "title": "睡覺", "completed": true },
+  ]
+)
 //取得唯一值
 const uniqueId = () => Date.now().toString(36) + Math.random().toString(36).substring(2, 9);
 
-//待辦事項新增
-const addEventHandler = todo => {
-    todos.value.push({ "id": uniqueId(), "title": todo, "completed": false })
+//待做事項新增
+const enterHandler = () => {
+  todos.value.push({ "id": uniqueId(), "title": newTodo.value, "completed": false })
+  newTodo.value = ''
 }
 
-
-
+//清除輸入的資料
+const deleteHandler = () => {
+  newTodo.value = ''
+}
 
 //刪除 todo
 const removeTodo = todo => {
-    //找到 todo 在 todos 中的索引值
-    const idx = todos.value.indexOf(todo)
+  //找到 todo 在 todos 中的索引值
+  const idx = todos.value.indexOf(todo)
 
-    //刪除 從idx這個位置刪除1筆資料
-    todos.value.splice(idx, 1)
+  //刪除 從idx這個位置刪除1筆資料
+  todos.value.splice(idx, 1)
 }
 
 //移除所有完成的工作
 const removeCompleted = () => {
-    for (let i = todos.value.length - 1; i >= 0; i--) {
-        if (todos.value[i].completed) {
-            todos.value.splice(i, 1)
-        }
+  for (let i = todos.value.length - 1; i >= 0; i--) {
+    if (todos.value[i].completed) {
+      todos.value.splice(i, 1)
     }
+  }
 }
 
 //計算還有幾個工作未完成
 const remaining = computed(() => {
-    const activeTodos = todos.value.filter(todo => !todo.completed)
-    return activeTodos.length
-    //store.count(activeTodos.length)
+  const activeTodos = todos.value.filter(todo => !todo.completed)
+  return activeTodos.length
 })
 
-//響應式資料改變了就把資料寫進localStorage
-//computed => 只做計算不要有 side effect
-//watch
-// watch('todos',(newTodos, oldTodos){
-
-// },{deep:true,immediate:true})
-
-//watchEffect
-watchEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos.value));
-
-    const activeTodos = todos.value.filter(todo => !todo.completed)
-    storeTodo.unfinishedQtyChange(activeTodos.length)
-})
 
 
 </script>
 
 <template>
-    <div class="row">
-        <div class="col-3"> </div>
-        <div class="col-6">
-            <h3>Todos Page</h3>
-            <TodoAdd @addEvent="addEventHandler"></TodoAdd>
-            <ul class="list-group mt-3">
-                <li v-for="todo in todos" :key="todo.id" class="list-group-item">
-                    <div class="d-flex justify-content-between">
-                        <div>
-                            <input v-model="todo.completed" class="form-check-input me-3" type="checkbox">
-                            <label class="form-check-label" :class="{ completed: todo.completed }">{{ todo.title
-                                }}</label>
-                        </div>
-                        <button @click="removeTodo(todo)" class="badge bg-danger rounded-pill border-0">X</button>
-                    </div>
+  <div class="row">
+    <div class="col-3"> </div>
+    <div class="col-6">
+      <h3>Todos Page</h3>
+      <input v-model="newTodo" @keyup.delete="deleteHandler" @keyup.enter="enterHandler" type="text"
+        class="form-control" autofocus autocomplete="off" placeholder="想要做甚麼?">
+      <ul class="list-group mt-3">
+        <li v-for="todo in todos" :key="todo.id" class="list-group-item">
+          <div class="d-flex justify-content-between">
+            <div>
+              <input v-model="todo.completed" class="form-check-input me-3" type="checkbox">
+              <label class="form-check-label" :class="{ completed: todo.completed }">{{ todo.title }}</label>
+            </div>
+            <button @click="removeTodo(todo)" class="badge bg-danger rounded-pill border-0">X</button>
+          </div>
 
-                </li>
+        </li>
 
-            </ul>
-            <TodoFooter @removeEvent="removeCompleted"></TodoFooter>
-        </div>
-        <div class="col-3"></div>
-
+      </ul>
+      <div class="mt-3 d-flex justify-content-between">
+        <strong class=" me-3">尚有 {{ remaining }} 個工作未完成</strong>
+        <button class="btn btn-warning me-3" @click="removeCompleted">清除完成工作</button>
+      </div>
     </div>
+    <div class="col-3"></div>
+
+  </div>
 </template>
 
 <style lang="css" scoped>
 .completed {
-    color: #949494;
-    text-decoration: line-through;
+  color: #949494;
+  text-decoration: line-through;
 }
 </style>
