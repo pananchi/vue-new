54d6616057e63094c53eb66353bd8fb8153a66f6
“Angel” <anm30256@gmail.com>
2025-07-17 17:11:56 +0800

    會員新增UI

---
+++ src/components/MemberAdd.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <form>
+      <div class="mb-3">
+        <label for="InputName" class="form-label">姓名</label>
+        <input type="text" class="form-control" id="InputName" name="name">
+      </div>
+      <div class="mb-3">
+        <label for="InputEmail1" class="form-label">電子郵件</label>
+        <input type="email" class="form-control" id="InputEmail1" name="email">
+      </div>
+      <div class="mb-3">
+        <label for="InputAge" class="form-label">年齡</label>
+        <input type="number" class="form-control" id="InputAge" name="age">
+      </div>
+      <div class="mb-3">
+        <label for="InputPassword" class="form-label">密碼</label>
+        <input type="password" class="form-control" id="InputPassword" name="password">
+      </div>
+      <div class="mb-3">
+        <label for="InputPasswordConfirm" class="form-label">密碼確認</label>
+        <input type="password" class="form-control" id="InputPasswordConfirm">
+      </div>
+      <div class="mb-3">
+        <label for="formFile" class="form-label">頭像</label>
+        <input class="form-control" type="file" id="formFile" name="file">
+      </div>
+      <button type="submit" class="btn btn-primary">新增</button>
+    </form>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
--- src/views/MemberView.vue
+++ src/views/MemberView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import MemberAdd from '@/components/MemberAdd.vue';
 import { ref } from 'vue';
 
 const members = ref([]);
@@ -8,7 +9,7 @@ const staticUrl = `${import.meta.env.VITE_STATICURL}/images`;
 
 //讀取所有會員資料
 const loadMember = async () => {
-  const response = await fetch(apiUrl);
+  const response = await fetch(apiUrl); //GET
   const datas = await response.json();
   members.value = datas;
 }
@@ -35,28 +36,36 @@ loadMember();
 <template>
   <div>
     <h3>會員管理</h3>
-    <table class="table table-bordered table-hover table-striped">
-      <thead>
-        <tr>
-          <th>頭像</th>
-          <th>姓名</th>
-          <th>電子郵件</th>
-          <th>年紀</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="member in members" :key="member.memberId">
-          <td><img :src="`${staticUrl}/${member.fileName}`" style="width: 150px;" /></td>
-          <td>{{ member.name }}</td>
-          <td>{{ member.email }}</td>
-          <td>{{ member.age }}</td>
-          <td>
-            <button @click="deleteHandler(member.memberId)" class="btn btn-danger">刪除</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="row">
+      <div class="col-9">
+        <table class="table table-bordered table-hover table-striped">
+          <thead>
+            <tr>
+              <th>頭像</th>
+              <th>姓名</th>
+              <th>電子郵件</th>
+              <th>年紀</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="member in members" :key="member.memberId">
+              <td><img :src="`${staticUrl}/${member.fileName}`" style="width: 150px;" /></td>
+              <td>{{ member.name }}</td>
+              <td>{{ member.email }}</td>
+              <td>{{ member.age }}</td>
+              <td>
+                <button @click="deleteHandler(member.memberId)" class="btn btn-danger"><i
+                    class="bi bi-trash"></i></button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="col-3">
+        <MemberAdd />
+      </div>
+    </div>
   </div>
 </template>
 
